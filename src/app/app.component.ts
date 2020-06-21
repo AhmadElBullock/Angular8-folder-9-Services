bc35c2f69c5ec8a4fadf36e11deff8efdd3227b9
AhmadElBullock <ahmadelbullock@gmail.com>
2020-06-21 22:38:03 +0200

    provide services in app.module + removing it from app.cmp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,7 @@ import { LoggingService } from './logging.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [LoggingService, AccountService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   accounts:{name: string, status: string}[] = [];
